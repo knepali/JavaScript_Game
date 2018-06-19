1647a67cea6d815cc764a60824f58ea409f24dd6
knepali <knepali@spartaglobal.com>
2018-06-19 15:47:03 +0100

    implemented multiclick disable for grid, so when clicked on grid, it doesn't override the image

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-var disabled = false;
 var box = document.getElementsByClassName('box'); //gets classname of grids
 for (var i = 0; i < box.length; i++) {
   if (i == 0 || i == 4 || i == 20 || i == 24) {
-      tiger();
+    tiger();
     i++;
   }
   if (i < 25) {
@@ -11,25 +10,30 @@ for (var i = 0; i < box.length; i++) {
 }
 
 function tiger() {
-    box[i].addEventListener('click', function() {
-      if(disabled){
-        return;
-      }
+  box[i].addEventListener('click', function(event) {
+    var clear = this.getAttribute('clear');
+    console.log(clear);
+    if (clear === 'empty') {
       var img = document.createElement("img");
       img.src = "images/tiger.jpg";
       this.appendChild(img);
       img.style.width = '60%';
-      disabled = true;
-    });
-  }
-
-
+      this.setAttribute('clear', 'occupied');
+      console.log(clear);
+    }
+  });
+}
 
 function goat() {
   box[i].addEventListener('click', function() {
-    var img = document.createElement("img");
-    img.src = "images/goat.jpg";
-    this.appendChild(img);
-    img.style.width = '60%';
+    var clear = this.getAttribute('clear');
+    if (clear === 'empty') {
+      var img = document.createElement("img");
+      img.src = "images/goat.jpg";
+      this.appendChild(img);
+      img.style.width = '60%';
+      this.setAttribute('clear', 'occupied');
+      console.log(clear);
+    }
   });
 }
